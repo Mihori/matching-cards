81b87829bca04424d82ffdac74119572f1c659a9
gabord <gabor.dobrozemszky@turbine.ai>
2021-04-06 15:09:48 +0200

    implement basic NavBar with some styling and functionality

--- src/App/NavBar.js
+++ src/App/NavBar.js
@@ -1,5 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import logo from './Images/logo.svg';
+
 const NavBar = () => {
-  return 'NavBar'
+
+  return (
+    <nav className='nav-bar'>
+      <NavLink to='/'>
+        <img className='nav-logo' src={logo} alt='' />
+      </NavLink>
+        <div className='nav-items'>
+          <label className='input-label'>Deck size: </label>
+          <select className='input' name='subject' onChange={(event) => console.log(event.target.value)}>
+            {[3,4,5,6,7,8,9,10].map((number, index) => <option key={index}>{number}</option>)}
+          </select>
+          <NavLink
+            className='new-game-button'
+            to={{ pathname: '/game'}}
+          >
+            New Game
+          </NavLink>
+        </div>
+    </nav>
+  );
 };
 
 export default NavBar;
