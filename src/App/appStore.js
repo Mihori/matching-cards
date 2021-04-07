fb5700b67692910eb95a3622fbba3847a24b8283
gabord <gabor.dobrozemszky@turbine.ai>
2021-04-07 18:32:01 +0200

    fix bug and refactor

--- src/App/appStore.js
+++ src/App/appStore.js
@@ -14,45 +14,31 @@ const setDeckSizeReducer = (state, action) => {
   };
 };
 
-const SET_CARD_CLICK = 'APP/SET_CARD_CLICK';
-export const setCardClick = (cardClick) => {
+const SET_TRY_COUNT = 'APP/SET_TRY_COUNT';
+export const setTryCount = (tryCount) => {
   return {
-    type: SET_CARD_CLICK,
-    cardClick,
+    type: SET_TRY_COUNT,
+    tryCount,
   };
 };
-const setCardClickReducer = (state, action) => {
+const setTryCountReducer = (state, action) => {
   return {
     ...state,
-    cardClick: action.cardClick,
+    tryCount: action.tryCount,
   };
 };
 
-const SET_CURRENT_TRIES = 'APP/SET_CURRENT_TRIES';
-export const setCurrentTries = (currentTries) => {
+const SET_BEST_RESULT = 'APP/SET_BEST_RESULT';
+export const setBestResult = (bestResult) => {
   return {
-    type: SET_CURRENT_TRIES,
-    currentTries,
+    type: SET_BEST_RESULT,
+    bestResult,
   };
 };
-const setCurrentTriesReducer = (state, action) => {
+const setBestResultReducer = (state, action) => {
   return {
     ...state,
-    currentTries: action.currentTries,
-  };
-};
-
-const SET_LEAST_FAILURES_RECORD = 'APP/SET_LEAST_FAILURES_RECORD';
-export const setLeastFailuresRecord = (leastFailuresRecord) => {
-  return {
-    type: SET_LEAST_FAILURES_RECORD,
-    leastFailuresRecord,
-  };
-};
-const setLeastFailuresRecordReducer = (state, action) => {
-  return {
-    ...state,
-    leastFailuresRecord: action.leastFailuresRecord,
+    bestResult: action.bestResult,
   };
 };
 
@@ -84,29 +70,28 @@ const setRevealedCardIndicesReducer = (state, action) => {
   };
 };
 
-const SET_REVEALED_CARD_TYPES = 'APP/SET_REVEALED_CARD_TYPES';
-export const setRevealedCardTypes = (revealedCardTypes) => {
+const SET_SOLVED_CARDS = 'APP/SET_SOLVED_CARDS';
+export const setSolvedCards = (solvedCards) => {
   return {
-    type: SET_REVEALED_CARD_TYPES,
-    revealedCardTypes,
+    type: SET_SOLVED_CARDS,
+    solvedCards,
   };
 };
-const setRevealedCardTypesReducer = (state, action) => {
+const setSolvedCardsReducer = (state, action) => {
   return {
     ...state,
-    revealedCardTypes: action.revealedCardTypes,
+    solvedCards: action.solvedCards,
   };
 };
 
 const initialState = () => {
   const initialState = {
     deckSize: 20,
-    currentTries: 0,
-    leastFailuresRecord: 0,
-    cardClick: 0,
+    tryCount: 0,
+    bestResult: undefined,
     cards: [],
     revealedCardIndices: [],
-    solvedCardTypes: []
+    solvedCards: []
   };
 
   return initialState;
@@ -114,12 +99,11 @@ const initialState = () => {
 
 const reducers = {
   [SET_DECK_SIZE]: setDeckSizeReducer,
-  [SET_CURRENT_TRIES]: setCurrentTriesReducer,
-  [SET_LEAST_FAILURES_RECORD]: setLeastFailuresRecordReducer,
-  [SET_CARD_CLICK]: setCardClickReducer,
+  [SET_TRY_COUNT]: setTryCountReducer,
+  [SET_BEST_RESULT]: setBestResultReducer,
   [SET_CARDS]: setCardsReducer,
   [SET_REVEALED_CARD_INDICES]: setRevealedCardIndicesReducer,
-  [SET_REVEALED_CARD_TYPES]: setRevealedCardTypesReducer,
+  [SET_SOLVED_CARDS]: setSolvedCardsReducer,
 };
 
 const appStore = createReducer (initialState(), reducers);
