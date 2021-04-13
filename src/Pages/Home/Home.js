1c3a33ebcb33e3d8729b1aff130a3f10e0b2cf6a
gabord <gabor.dobrozemszky@turbine.ai>
2021-04-13 18:04:06 +0200

    add game rules to home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,6 +7,11 @@ const Home = () => {
   return (
     <div className='home-container'>
       <h1>Splendex Memory Game</h1>
+      <div className='rules'>
+        Select a deck size of your preference, then start a new game. <br />
+        Try to find all matching cards with the least tries possible! <br />
+        Your best results are going to be stored for each deck size.
+      </div>
       <label className='input-label'>Deck size: </label>
       <div style={{ marginBottom: '24px' }}><DeckSizeSelector /></div>
       <NewGameButton text='start new game'/>
