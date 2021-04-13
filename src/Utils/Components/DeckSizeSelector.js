d8d896a92714c32e7766b834bd59d11390b1555e
gabord <gabor.dobrozemszky@turbine.ai>
2021-04-13 18:18:17 +0200

    save deckSize to localStorage when it changes

--- src/Utils/Components/DeckSizeSelector.js
+++ src/Utils/Components/DeckSizeSelector.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setDeckSize } from '../../App/appStore';
-
-const deckSizeValues = [6, 8, 10, 12, 14, 16, 18, 20];
+import { deckSizeValues, setDeckSize } from '../../App/appStore';
 
 const DeckSizeSelector = () => {
   const dispatch = useDispatch();
+  const appState = useSelector(state => state);
   const deckSize = useSelector(state => state.deckSize);
 
+  const onDeckSizeChange = (newSize) => {
+    window.localStorage.setItem('memoryGameState', JSON.stringify({
+      ...appState,
+      deckSize: newSize
+    }))
+    dispatch(setDeckSize(newSize));
+  }
+
   return (
-    <select className='input' name='deck-size-selector' value={deckSize} onChange={(event) => dispatch(setDeckSize(event.target.value))}>
+    <select className='input' name='deck-size-selector' value={deckSize} onChange={(event) => onDeckSizeChange(event.target.value)}>
       {deckSizeValues.map((number, index) => <option key={index}>{number}</option>)}
     </select>
   )
